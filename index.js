7ffca4813be9675a79ff04c03d4dd3277f750d8a
noreplyMailSender <155156153+noreplyMailSender@users.noreply.github.com>
2023-12-29 17:14:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const corsOptions = {
   origin: 'https://tool-sphere.github.io/',
   optionsSuccessStatus: 200,
 };
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.send('Request Origin:', req.get('Origin'));
+  next();
+});
+/*app.use(cors(corsOptions));
 //app configuration 
 app.get("*", async(req, res) => {
 res.send("Hii");
-});
+});*/
 //endd
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
