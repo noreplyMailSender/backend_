8fed167ebe6b0271196a27850b90e4e464226b31
noreplyMailSender <155156153+noreplyMailSender@users.noreply.github.com>
2023-12-29 17:04:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const corsOptions = {
+  origin: 'https://tool-sphere.github.io/',
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+//app configuration 
+
+//endd
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
