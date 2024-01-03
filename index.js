a9132fe535c204a4a9e163f4d8ba4f2476b64eda
noreplyMailSender <155156153+noreplyMailSender@users.noreply.github.com>
2024-01-03 10:22:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const Gradio = require("./cjs gradio client.js");
+const translator = require("./translate/inded.js");
 const app = express();
 const corsOptions = {
   origin: 'https://tool-sphere.github.io',
